09f1c118af9caa7c1b1d967acdc59810919ea393
Riley <rbilchik@ufl.edu>
2022-04-18 18:40:04 -0400

    updated code with correct search and start of resize

--- src/HashTable.h
+++ src/HashTable.h
@@ -1,12 +1,10 @@
+#pragma once
+
 #include <iostream>
 #include <list>
 #include <vector>
 
 using namespace std;
-/*struct Actor{
-  int key;
-  string name;
-};*/
 
 class HashTable{
     private:
@@ -22,70 +20,133 @@ class HashTable{
         
         const int actorTotal = 11456063;
   
-        double loadFactor = 0.8;
+        double threshold = 0.8;
         
+        double loadFactor;
+        
+        //gets the hash of a key by dividing a unique key by the total number of buckets in the table
         int getHash(int key){
             return key % total;
         }
         
     public:
-        /*HashTable(){
-            total = actorTotal / loadFactor;
+        //base constructor of HashTable, used for known amount of items
+        HashTable(){
+            //setting the total number of buckets within the table to be just under the threshold required for a resize
+            total = (actorTotal / threshold) + 1;
             table = new list<Actor*>[total];
-        }*/
+        }
         
+        //constructor used when creating a table that needs to be modular
         HashTable(int n){
+            //total number of buckets is set to a desired initial number
             total = n;
             table = new list<Actor*>[total];
         }
         
-        /*HashTable(){
-            total = actorTotal / loadFactor;
-            table = new list<Actor*>[total];
-        }*/
+        //returns the number of buckets in the table
+        int getTotal(){
+            return total;
+        }
         
-        /*double getLoadFactor(){
-            double loadFactor;
-            double count;
+        //function used to calculate the load size of the table
+        double getLoadFactor(){
+            double count = 0;
             
+            list<Actor*>::iterator it;
+            for(int x = 0; x < total; x++){
+                //x is the index (hash) of the bucket
+                for(it = table[x].begin(); it != table[x].end(); ++it){
+                    Actor* temp = *it;
+                    //check to see if the bucket is filled or empty by seeing if there is an actor object in the bucket or if it is NULL
+                    if(temp != NULL){
+                        count++;
+                    }
+                }
+            }
+            //load factor is calculated by dividing the total number of buckets filled by the total number of buckets
             loadFactor = count / total;
             return loadFactor;
-        }*/
+        }
         
+        void resize(){
+            total *= 2;
+            
+            /*list<Actor*>::iterator it;
+            for(int x = 0; x < total; x++){
+                for(it = table[x].begin(); it != table[x].end(); ++it){
+                    Actor* temp = *it;
+                    if(temp != NULL){
+                        count++;
+                    }
+                }
+            }*/
+        }
+        
+        //function used to insert an actor into the table
         void insert(int Key, string Name, string mList){
             Actor* actor = new Actor();
             actor->key = Key;
             actor->name = Name;
             actor->movieList = mList;
-            //cout << actor->key << " " << actor->name << " " << getHash(Key) << endl;
+            //we push back the actor to the key index we get when we call the hash function of their key
             table[getHash(Key)].push_back(actor);
         }
         
+        //removes an actor from the table
         void removeA(int Key, string Name){
             int x = getHash(Key);
             
             list<Actor*>::iterator it;
+            //we check the list at the hashed index for the desired name of the actor using an iterator
             for(it = table[x].begin(); it != table[x].end(); ++it){
                 Actor* temp = *it;
+                //checks to see if the actors name the iterator is currently pointing to matches the desired name
                 if(temp->name == Name){
-                    //cout << temp->name << " ";
+                    //if it matches the for loop breaks, 
                     break;
                 }
             }
             
+            //if the loop breaks, the iterator will equal the desired actor, and as long as we are not at the end of the list at the hashed index, we delete that actor
             if(it != table[x].end()){
                 table[x].erase(it);
             }
         }
         
+        //function used to find an actor based on their unique key and name
         bool findA(int Key, string Name){
+            //much of this structure is very similar to the remove function
+            int x = getHash(Key);
+            
+            list<Actor*>::iterator it;
+            for(it = table[x].begin(); it != table[x].end(); ++it){
+                //checks to see if the name attatched to the temp actor is the name we are looking for
+                Actor* temp = *it;
+                if(temp->name == Name){
+                    break;
+                }
+            }
+            
+            //if the temp actors name matches, we return true
+            if(it != table[x].end()){
+                return true;
+            }
+            
+            //if the loop is never broken, we did not find the actor, so we return false
+            return false;
+        }
+        
+        //function used to find an actor using only their unique id
+        bool findA(int Key){
+            //this function is the same as the function above
             int x = getHash(Key);
             
             list<Actor*>::iterator it;
             for(it = table[x].begin(); it != table[x].end(); ++it){
                 Actor* temp = *it;
-                if(temp->name == Name){
-                    //cout << temp->name << " ";
+                //instead of checking for actor names we are now comparing actor keys
+                if(temp->key == Key){
                     break;
                 }
             }
@@ -97,6 +158,7 @@ class HashTable{
             return false;
         }
         
+        //function used to print the contents of the table
         void printAll(){
             list<Actor*>::iterator it;
             for(int i = 0; i < total; i++){
@@ -112,26 +174,38 @@ class HashTable{
 };
 
 /*int main(){
-    HashTable ht(2);
-    //HashTable ht();
+    //HashTable ht;
+    HashTable ht(3);
     
     ht.insert(1, "Harry Potter" , "Sorcerers Stone");
     ht.insert(1, "Larry King", "Late Night");
     ht.insert(0, "Larry Lobster", "SpongeBob");
     
-    ht.printAll();
-    //cout << endl << ht.getLoadFactor() << endl;
-    cout << ht.findA(1, "Larry King") << endl;
-    
-    ht.removeA(1, "Larry King");
-    
-    ht.printAll();
-    
-    cout << ht.findA(1, "Harry Potter") << endl;
-    cout << ht.findA(1, "Larry King");
-    
     //ht.printAll();
     //cout << endl << ht.getLoadFactor() << endl;
+    //cout << ht.findA(1, "Larry King") << endl;
     
+    //ht.removeA(1, "Larry King");
+    cout << ht.findA(0);
+    
+    //ht.printAll();
+    
+    //cout << ht.findA(1, "Harry Potter") << endl;
+    //cout << ht.findA(1, "Larry King") << endl;
+    
+    //cout << ht.getTotal() << endl;
+    //cout << ht.getLoadFactor() << endl;
+    /*ht.resize();
+    cout << ht.getTotal() << endl;*/
+    //ht.printAll();
+    //cout << endl << ht.getLoadFactor() << endl;
+    //int total = ht.actorTotal / ht2.loadFactor;
+    //cout << total;
+    
+    /*HashTable ht(total);
+    ht.insert(1, "Harry Potter" , "Sorcerers Stone");
+    ht.insert(1, "Larry King", "Late Night");
+    ht.insert(0, "Larry Lobster", "SpongeBob");
+    ht.printAll();
     return 0;    
 }*/
